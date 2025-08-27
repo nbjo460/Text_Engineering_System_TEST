31972ead25a36cb4d2c4e00e50d94553ef57b50b
YeruhamMendelson <y0583207549@gmail.com>
2025-08-27 13:53:05 +0300

    class of DAL_mongo

---
+++ mongodb_tools/DAL_mongodb.py
@@ -0,0 +1,72 @@
+from pymongo import MongoClient
+
+
+
+class DAL_mongo:
+
+    def __init__(self, prefix, host, database, collection, user= None, password= None):
+        self.prefix = prefix
+        self.host = host
+        self.database = database
+        self.collection = collection
+        self.user = user
+        self.password = password
+        self.URI = self.get_URI()
+        self.client = None
+
+
+    def get_URI(self):
+        if self.user and self.password:
+            URI = f"{self.prefix}://{self.user}:{self.password}@{self.host}::27017"
+        else:
+            URI = f"{self.prefix}://{self.host}:27017"
+
+        return URI
+
+
+    def open_connection(self):
+        try:
+            self.client = MongoClient(self.URI)
+            self.client.admin.command("ping")
+            return True
+        except Exception as e:
+            self.client = None
+            print("Error: ", e)
+            return False
+
+
+    def get_documents(self, num_skip, num_limit, sort_by_filed):
+        if self.client:
+            db = self.client[self.database]
+            collection = db[self.collection]
+            data = collection.find({}, {"_id": 0}).skip(num_skip).limit(num_limit).sort({sort_by_filed: 1})
+            return list(data)
+
+
+
+    def insert_one(self, data):
+        if self.client:
+            db = self.client[self.database]
+            collection = db[self.collection]
+            results = collection.insert_one(data)
+            return results
+
+
+
+
+    def close_connection(self):
+        if self.client:
+            self.client.close()
+
+
+if __name__ == "__main__":
+    DAL_mongo = DAL_mongo(prefix="mongodb+srv",
+                          host="cluster0.6ycjkak.mongodb.net/",
+                          database="IranMalDB",
+                          collection="tweets",
+                          user="IRGC_NEW",
+                          password="iran135")
+    DAL_mongo.open_connection()
+    documents = DAL_mongo.get_documents(5, 5, "CreateDate")
+    print(documents)
+    DAL_mongo.close_connection()

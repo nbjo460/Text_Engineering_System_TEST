a829d9db40ead32bcd3b2a93035096b734eea2d4
menachem yarhi <nbjo460@gmail.com>
2025-08-27 17:32:21 +0300

    added main and manager files

---
+++ preprocessor/Manager.py
@@ -0,0 +1,7 @@
+import mongodb_tools.DAL_mongodb as dal
+from kafka_tools.consumer import Consumer
+from kafka_tools.producer import Producer
+
+class Manager:
+    def __init__(self):
+        pass

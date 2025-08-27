758d63777af7745d0b4c8ca4f827561ac51610ef
menachem yarhi <nbjo460@gmail.com>
2025-08-27 17:35:03 +0300

    config parameters file added

--- preprocessor/Manager.py
+++ preprocessor/Manager.py
@@ -1,7 +1,10 @@
-import mongodb_tools.DAL_mongodb as dal
 from kafka_tools.consumer import Consumer
 from kafka_tools.producer import Producer
+from mongodb_tools.DAL_mongodb import DAL_mongo
+
 
 class Manager:
     def __init__(self):
-        pass
+        dal = DAL_mongo()
+        consumer = Consumer()
+        producer = Producer()

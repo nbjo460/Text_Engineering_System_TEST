e622bf20dc2132b5e8baa1d0a69b9b36001a3b58
menachem yarhi <nbjo460@gmail.com>
2025-08-27 11:38:57 +0300

    clean func

--- utils/cleaner.py
+++ utils/cleaner.py
@@ -3,3 +3,10 @@ import pandas as pd
 class Cleaner:
     def __init__(self, df : pd):
         self.df = df
+
+    def clean(self):
+        """
+        Apply all the clean options, to one func.
+        :return: DataFrame
+        """
+        pass

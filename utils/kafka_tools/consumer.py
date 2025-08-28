37a145261afedd001d474c44bf415b597724484e
YeruhamMendelson <y0583207549@gmail.com>
2025-08-28 09:03:27 +0300

    many topics in consumer

--- utils/kafka_tools/consumer.py
+++ utils/kafka_tools/consumer.py
@@ -4,14 +4,14 @@ import json
 
 class Consumer:
 
-    def __init__(self, server_uri, topic):
+    def __init__(self, server_uri, *topics):
         self.server_uri = server_uri
         self.consumer_events = None
-        self.topic = topic
+        self.topics = topics
 
 
     def run_consumer_events(self):
-        self.consumer_events = KafkaConsumer(self.topic,
+        self.consumer_events = KafkaConsumer(*self.topics,
                                  value_deserializer=lambda m: json.loads(m.decode('ascii')),
                                  group_id='text-group',
                                  bootstrap_servers=[self.server_uri])
@@ -24,7 +24,7 @@ class Consumer:
                 print(f"offset:  {message.offset}")
                 print(f"topic:  {message.topic}")
 
-                yield message.value
+                yield message
 
 
 if __name__ == '__main__':
